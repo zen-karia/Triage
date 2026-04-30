3eb6b12eac8c4bd8e5ab768bec34f0253c238636
zen-karia <149807516+zen-karia@users.noreply.github.com>
2026-04-30 10:28:46 -0400

    Add DynamoDB orders table to PersistenceStack, move build output to build/>

--- lib/persistence-stack.ts
+++ lib/persistence-stack.ts
@@ -1,8 +1,13 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
+import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
 
 export class PersistenceStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
+
+        const orderTable = new Table(this, "OrderRecords", {
+            partitionKey: { name: "orderID", type: AttributeType.STRING}
+        });
     }
 }
