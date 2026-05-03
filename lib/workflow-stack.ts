549c6195a519065e68409c1bed7440f1df41e45d
zen-karia <149807516+zen-karia@users.noreply.github.com>
2026-05-03 17:46:32 -0400

    Add retry policies and error handlers to all Step Functions tasks; update README

--- lib/workflow-stack.ts
+++ lib/workflow-stack.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
-import { StateMachine, Chain } from 'aws-cdk-lib/aws-stepfunctions';
+import { StateMachine, Chain, Fail } from 'aws-cdk-lib/aws-stepfunctions';
 import { LambdaInvoke } from 'aws-cdk-lib/aws-stepfunctions-tasks';
 import { Table } from 'aws-cdk-lib/aws-dynamodb';
 
@@ -50,25 +50,58 @@ export class WorkflowStack extends cdk.Stack {
             }
         });
 
+        const failstate = new Fail(this, 'OrderFailed', {
+            cause: 'Step failed after retries',
+            error: 'OrderProcessingError'
+        });
+
         const aiFraudScoreTask = new LambdaInvoke(this, 'AIFraudScoreTask', {
             lambdaFunction: aiFraudScoreHandler,
             outputPath: '$.Payload'
         })
+
+        aiFraudScoreTask.addRetry({ 
+            maxAttempts: 2,
+            interval: cdk.Duration.seconds(2),
+            backoffRate: 2
+        });
+        aiFraudScoreTask.addCatch(failstate);
 
         const checkInventoryTask = new LambdaInvoke(this, 'CheckInventoryTask', {
             lambdaFunction: checkInventoryHandler,
             outputPath: '$.Payload'
         })
 
+        checkInventoryTask.addRetry({ 
+            maxAttempts: 2,
+            interval: cdk.Duration.seconds(2),
+            backoffRate: 2
+        });
+        checkInventoryTask.addCatch(failstate);
+
         const processPaymentTask = new LambdaInvoke(this, 'ProcessPaymentTask', {
             lambdaFunction: processPaymentHandler,
             outputPath: '$.Payload'
         })
 
+        processPaymentTask.addRetry({ 
+            maxAttempts: 2,
+            interval: cdk.Duration.seconds(2),
+            backoffRate: 2
+        });
+        processPaymentTask.addCatch(failstate);
+
         const fulfillOrderTask = new LambdaInvoke(this, 'FulfillOrderTask', {
             lambdaFunction: fulfillOrderHandler,
             outputPath: '$.Payload'
         })
+
+        fulfillOrderTask.addRetry({ 
+            maxAttempts: 2,
+            interval: cdk.Duration.seconds(2),
+            backoffRate: 2
+        });
+        fulfillOrderTask.addCatch(failstate);
 
         const definition = Chain.start(aiFraudScoreTask).next(checkInventoryTask).next(processPaymentTask).next(fulfillOrderTask);
 
